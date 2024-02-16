80a0996e735656bf99e271ef9d99b308b234cb02
maraimmarco <mariammarco646@gmail.com>
2024-02-17 00:26:55 +0200

    this file create with GUI and notification

--- test.sh
+++ test.sh
@@ -1,113 +1,48 @@
 #!/bin/bash
-#create database ->dir done 
-#connect to database schema doneeeeeeeeeee
-#create table database -> file
-#list table -> list only files
-#connect database doneeeeeeeee
-#select data from table 
-#isnert and update from database 
-#handle all input and cirtical 
-#priamry key -> unique and not null 
 
-select option in createDB connectDB
-do 
-    case $option in
-        "createDB")
-            read -p "please enter data base name to create " DBNMAE
-            #if string empty
-            if [[ -z $DBNMAE ]]
-            then 
-            echo "input is empty"
-            continue
-            fi 
+echo hello
 
-            if [[ ! -e $DBNMAE ]]
-            then 
-            pwd
-            mkdir ${DBNMAE/}
-            echo database created successfully 
-            else 
-                echo "database already exist" 
-            fi 
-            ;;
-        "connectDB")
-            read -p "please enter database name " conDB
-            #if string empty
-            if [[ -z $conDB ]]
-            then 
-            echo "input is empty"
-            continue
+option=$(zenity --list --title="Select Option" --column="Options" "createDB" "connectDB" --width=250 --height=200 --hide-header)
+
+case $option in
+    "createDB")
+        DBNAME=$(zenity --entry --title="Create DataBase" --text="please enter database name to create:" --entry-text="New DataBase")
+        # Check if the input is empty
+        if [[ -z $DBNAME ]]; then
+            echo "Input is empty"
+            zenity --notification --text="Input Is Empty"
+        else
+            if [[ ! -e $DBNAME ]]; then
+                mkdir "$DBNAME"
+                echo "Database created successfully"
+                zenity --notification --text="Database created: $DBNAME"
+            else
+                echo "Database already exists"
+                zenity --notification --text="Database already exist"
             fi
-            if [[ -e $conDB ]]
-            then
-            cd  $conDB
-            #connect on database         
-            # Define variables
-            user_name="casestudy"
-            password="root"
-            host_name="MariamMarcos"
-            port="1521"
-            service_name="XE"
-            sqlplus -s "${user_name}/${password}@${host_name}:${port}/${service_name}" <<EOF
-            --select * from instructor;
-EOF
-            echo "connected done "
-            echo "DataBase connected successfully"
-            #need to call function here 
-            select option in createTB listTB
-        do 
-            case $option in 
-                "createTB")
-                    read -p "please enter table name :" TBname
-                    read -p "please enter column number: " colNumber
-                    line=""
-                    for ((i=0;i<$colNumber;i++))
-                    do
-                        read -p "enter column name:" colName
-                        line+=:$colName
-                        read -p "enter column data type: " colDatatype
-                        line+=:$colDatatype
-                        
-                    done
-                    echo "table is created"
-                    touch $TBname
-                    echo $line>>$TBname
-                ;;
-                "listTB")
-                    result=cat metastd | cut -d: -f1
-                ;;
-            esac
-        done 
-            else 
-            echo "database is not exist"
+        fi
+        ;;
+    "connectDB")
+        echo "Connect to database"
+        conDB=$(zenity --entry --title="Connect to DataBase" --text="Please enter database name:")
+        # Check if the input is empty
+        if [[ -z $conDB ]]; then
+            echo "Input is empty"
+            zenity --notification --text="Input is Empty"
+        else
+            if [[ -e $conDB ]]; then
+                cd "$conDB" || exit
+                echo "Connected to $conDB"
+                zenity --notification --text="Database connected: $conDB"
+            else
+                echo "Database does not exist"
+                zenity --notification --text="Database Doesnot Exist"
             fi
-            ;;
-    esac
-done 
-#craete tables and list tables 
-displayDatabaseOption(){
-    clear
-    select option in createTB listTB
-        do 
-            case $option in 
-                "createTB")
-                    read -p "please enter table name :" TBname
-                    read -p "please enter column number: " colNumber
-                    line=""
-                    for ((i=0;i<$colNumber;i++))
-                    do
-                        read -p "enter column name:" colName
-                        line+=:$colName
-                        read -p "enter column data type: " colDatatype
-                        line+=:$colDatatype
-                        echo $line>>$TBname
-                    done
-                    echo "table is created"
-                    touch $TBname
-                ;;
-                "listTB")
-                    result=cat metastd | cut -d: -f1
-                ;;
-            esac
-        done 
-}
+        fi
+        ;;
+    *)
+        echo "Invalid option"
+        zenity --notification --text="INVAILD OPTION"
+        ;;
+esac
+
